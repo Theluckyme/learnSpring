4a94807fce8c5e0c4062c8d30629e23ffac55017
Subham Kumar  Rana <92314926+Theluckyme@users.noreply.github.com>
2025-02-12 01:57:44 +0530

    Add files via upload

---
+++ src/main/java/com/subh/sqlcheck/GetDto/RequestDto.java
@@ -0,0 +1,14 @@
+package com.subh.sqlcheck.GetDto;
+
+public class RequestDto {
+	private int userId;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	
+}
