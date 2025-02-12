336b63c8097986e73b7e6b41314b7cf327036acc
Subham Kumar  Rana <92314926+Theluckyme@users.noreply.github.com>
2025-02-12 08:24:20 +0530

    Delete src directory

--- src/main/java/com/subh/sqlcheck/services/UserServices.java
+++
@@ -1,8 +0,0 @@
-package com.subh.sqlcheck.services;
-
-import com.subh.sqlcheck.GetDto.RequestDto;
-import com.subh.sqlcheck.GetDto.ResponseDto;
-
-public interface UserServices {
-	ResponseDto getDetail(RequestDto request);
-}
